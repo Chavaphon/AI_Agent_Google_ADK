e3aff5c91d777507071b8182a508309bdc04638c
Bardlightyear <Chavaphon177@gmail.com>
2026-09-03 19:01:16 +0700

    test case

--- my_first_agent/test_agent.py
+++ my_first_agent/test_agent.py
@@ -4,8 +4,10 @@ from google.adk.runners import InMemoryRunner
 from agent import root_agent
 from dotenv import load_dotenv
 
+load_dotenv()
+
 @pytest.mark.asyncio
 async def test_agent_execution():
     runner = InMemoryRunner(agent=root_agent)
     response = await runner.run_debug("Calculate 10 + 5", verbose=False)
-    assert "15" in response.text
+    assert "15" in str(response[-1])
