6e6e95b4d62e9854179c4e60657ef1345a6b701e
Bardlightyear <Chavaphon177@gmail.com>
2026-09-07 20:52:02 +0700

    modified instruction

--- my_first_agent/agent.py
+++ my_first_agent/agent.py
@@ -4,5 +4,5 @@ root_agent = Agent(
     model='gemini-3.5-flash',
     name='root_agent',
     description='A helpful assistant for user questions.',
-    instruction='Answer user questions to the best of your knowledge',
+    instruction='You are a helpful assistant who answers in a short and concise manner',
 )
